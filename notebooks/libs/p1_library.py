0ec83e9b1ee6c5f78b6eb662deb57b2273652552
a-altman <altmanandrew03@gmail.com>
2022-04-03 09:42:12 -0400

    worked on turning csv to dataframe code into function

---
+++ notebooks/libs/p1_library.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pathlib import Path
+import numpy as np
+import os
+
+def chemical_filter(file_path,chemical_list):
+    chemical_data = []
+
+    for filename in os.listdir(file_path):
+        if filename.endswith(".csv"):
+            csv_data = pd.read_csv(filename, parse_dates=True, infer_datetime_format=True)
+            for item in chemical_list:
+                chemicals_filtered = csv_data[csv_data['CHEMICAL_NAME'] == item]
+                chemicals_filtered = chemicals_filtered.iloc[: , 1:]
+                chemical_data.append(chemicals_filtered)
+
+    chemicals_df = pd.concat(chemical_data)
+    return chemicals_df
