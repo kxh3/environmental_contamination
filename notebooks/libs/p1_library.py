58cfa1a1af25f9134afe1f22a901cb48e86c0226
a-altman <altmanandrew03@gmail.com>
2022-04-04 19:09:18 -0400

    worked out bugs in .py file to call chemical filter function which loops through cleandata csvs and concats dataframe of select chemical list

--- notebooks/libs/p1_library.py
+++ notebooks/libs/p1_library.py
@@ -3,16 +3,15 @@ from pathlib import Path
 import numpy as np
 import os
 
-def chemical_filter(file_path,chemical_list):
+def chemical_filter(file_path, chemical_list):
     chemical_data = []
 
     for filename in os.listdir(file_path):
         if filename.endswith(".csv"):
-            csv_data = pd.read_csv(filename, parse_dates=True, infer_datetime_format=True)
+            csv_data = pd.read_csv(file_path +'/'+ filename, parse_dates=True, infer_datetime_format=True)
             for item in chemical_list:
                 chemicals_filtered = csv_data[csv_data['CHEMICAL_NAME'] == item]
                 chemicals_filtered = chemicals_filtered.iloc[: , 1:]
                 chemical_data.append(chemicals_filtered)
 
-    chemicals_df = pd.concat(chemical_data)
-    return chemicals_df
+    return pd.concat(chemical_data)
